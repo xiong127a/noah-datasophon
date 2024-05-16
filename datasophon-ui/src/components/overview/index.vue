564b36e29c087ea08688327cef4797115bb1e630
lixuting <lixuting@norinaviation.com>
2024-05-16 16:36:40 +0800

    地址栏带过期ticket重定向的问题调试

--- datasophon-ui/src/components/overview/index.vue
+++ datasophon-ui/src/components/overview/index.vue
@@ -1,29 +1,3 @@
-<!--
-/*
- *
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
-
-
- * @describe: 
- * @Date: 2022-06-21 16:31:17
- * @LastEditTime: 2022-09-26 17:07:58
- * @FilePath: \ddh-ui\src\components\overview\index.vue
--->
 <template>
   <div>
     <iframe v-if="reloadIframe" id="iframe" :src="dashboardUrl" class="overview-iframe" width="100%" frameborder="0"></iframe>
@@ -38,16 +12,23 @@ export default {
       default: true
     }
   },
-  computed: {
-  },
+
   data() {
     return {
       iframeSrc1:
         "http://172.31.96.15:3000/d/_4gf-qOZz/1-zong-lan?orgId=1&refresh=5s&fromType=et",
     };
   },
+
+  computed: {
+  },
+
+  created() {
+    console.log(this.dashboardUrl)
+  }
 };
 </script>
+
 <style lang="less" scoped>
 .overview-iframe {
   position: absolute;
