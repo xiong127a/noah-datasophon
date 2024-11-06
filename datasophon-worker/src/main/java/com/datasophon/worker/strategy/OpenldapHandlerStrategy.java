444256e4430748413e30838a0408c7a0ce932864
xiongzhijie <xiongzhijie@norinaviation.com>
2024-11-06 17:19:00 +0800

    nebulagraph graph修改角色为master

--- datasophon-worker/src/main/java/com/datasophon/worker/strategy/OpenldapHandlerStrategy.java
+++ datasophon-worker/src/main/java/com/datasophon/worker/strategy/OpenldapHandlerStrategy.java
@@ -21,36 +21,7 @@ public class OpenldapHandlerStrategy extends AbstractHandlerStrategy implements 
         String workPath = Constants.INSTALL_PATH + Constants.SLASH + command.getDecompressPackageName();
 
         if (command.getCommandType().equals(CommandType.INSTALL_SERVICE)) {
-            String cpuArchitecture = ShellUtils.getCpuArchitecture();
-
-            // 安装依赖
-            logger.info("start install libtool");
-            ShellUtils.exceShell("yum localinstall -y " + workPath + "/*rpm");
-            // 安装服务
-            logger.info("start install openldap");
-            ShellUtils.exceShell("echo \"" + workPath + "/db/lib/\" > /etc/ld.so.conf");
-            if ("aarch64".equals(cpuArchitecture)) {
-                ShellUtils.exceShell("echo \"" + workPath + "/db/lib/\" > /etc/ld.so.conf");
-            }
-            ShellUtils.exceShell("ldconfig -v");
-            String configureCommand = "sh " + workPath + "/configure " +
-                    "--prefix=" + workPath + " " +
-                    "--enable-syslog " +
-                    "--enable-modules " +
-                    "--enable-debug " +
-                    "--enable-dynamic " +
-                    "--enable-rlookups " +
-                    "--enable-slapd " +
-                    "--enable-crypt " +
-                    "--enable-spasswd " +
-                    "--enable-memberof=mod " +
-                    "--enable-refint=mod " +
-                    "--with-tls " +
-                    "CPPFLAGS=-I" + workPath + "/db/include " +
-                    "LDFLAGS=-L" + workPath + "/db/lib";
-            ShellUtils.exceShell(configureCommand);
-            logger.info("start make && make install");
-            ShellUtils.exceShell("make && make install");
+            //String cpuArchitecture = ShellUtils.getCpuArchitecture();
 
             // 开启日志
             ShellUtils.exceShell("echo \"local4.* "+ workPath + "/var/slapd.log\" >> /etc/rsyslog.conf");
@@ -60,7 +31,6 @@ public class OpenldapHandlerStrategy extends AbstractHandlerStrategy implements 
             logger.info("start OpenLDAP");
             ShellUtils.exceShell("ln -s " + workPath + "/bin/* /usr/bin/");
             ShellUtils.exceShell("ln -s " + workPath + "/sbin/* /usr/sbin/");
-            ShellUtils.exceShell("cp -f " + workPath + "/slapd.conf " + workPath + "/etc/openldap");
             ShellUtils.exceShell("sh " + workPath + "/setup_tls.sh");
             logger.info("init success");
 
