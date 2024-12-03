e649ff0a02cbbf92b0c342a9d15b7e3a9777c974
xiongzhijie <xiongzhijie@norinaviation.com>
2024-12-03 16:02:22 +0800

    feat: dolphinscheduler pvm x86\arm集成、容器集成

---
+++ datasophon-worker/src/main/java/com/datasophon/worker/strategy/DSHandlerStrategy.java
@@ -0,0 +1,53 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.datasophon.worker.strategy;
+
+import cn.hutool.core.io.FileUtil;
+import com.datasophon.common.Constants;
+import com.datasophon.common.cache.CacheUtils;
+import com.datasophon.common.command.ServiceRoleOperateCommand;
+import com.datasophon.common.enums.CommandType;
+import com.datasophon.common.utils.ExecResult;
+import com.datasophon.common.utils.ShellUtils;
+import com.datasophon.worker.handler.ServiceHandler;
+import com.datasophon.worker.utils.KerberosUtils;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DSHandlerStrategy extends AbstractHandlerStrategy implements ServiceRoleStrategy {
+
+    public DSHandlerStrategy(String serviceName, String serviceRoleName) {
+        super(serviceName, serviceRoleName);
+    }
+
+    @Override
+    public ExecResult handler(ServiceRoleOperateCommand command) throws SQLException, ClassNotFoundException {
+        ExecResult startResult = new ExecResult();
+        String workPath = Constants.INSTALL_PATH + Constants.SLASH + command.getDecompressPackageName();
+        ServiceHandler serviceHandler = new ServiceHandler(command.getServiceName(), command.getServiceRoleName());
+        if (command.getCommandType() == CommandType.INSTALL_SERVICE) {
+            logger.info("init ds");
+            ShellUtils.exceShell("bash " + workPath + "/tools/bin/upgrade-schema.sh");
+        }
+        startResult = serviceHandler.start(command.getStartRunner(), command.getStatusRunner(),
+                command.getDecompressPackageName(), command.getRunAs());
+        return startResult;
+    }
+
+}
