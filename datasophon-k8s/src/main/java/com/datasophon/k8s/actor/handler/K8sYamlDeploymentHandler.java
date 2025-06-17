ac1a381a418cfd4d2fb8e432bd567cbbd1be9797
任相鹏 <renxiangpeng@norinaviation.com>
2025-06-17 14:15:29 +0800

    feat(k8s): ✨ 添加对JournalNode配置的支持
    在K8sYamlDeploymentHandler.java中新增了对HDFS JournalNode目录配置的支持，通过添加populateDataWithConfig方法调用将dfs.journalnode.edits.dir映射到journalnodeDir。
    
    这将完善HDFS集群部署时对JournalNode服务的配置项支持。

--- datasophon-k8s/src/main/java/com/datasophon/k8s/actor/handler/K8sYamlDeploymentHandler.java
+++ datasophon-k8s/src/main/java/com/datasophon/k8s/actor/handler/K8sYamlDeploymentHandler.java
@@ -311,6 +311,7 @@ public class K8sYamlDeploymentHandler {
             populateDataWithConfig(configFileMap, "dfs.namenode.shared.edits.dir", "nn_shared_edits_dir");
             populateDataWithConfig(configFileMap, "dfs.namenode.checkpoint.dir", "snn_checkpoint_dir");
             populateDataWithConfig(configFileMap, "dfs.datanode.data.dir", "dn_data_dir");
+            populateDataWithConfig(configFileMap, "dfs.journalnode.edits.dir", "journalnodeDir");
             populateDataWithConfig(configFileMap, "ha.zookeeper.quorum", "zkQuorum");
         }
         if ("KAFKA".equals(serviceName)) {
