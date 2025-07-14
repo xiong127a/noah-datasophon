7ca574507cbf7fcb79a6033a873a37bdda52c650
xiongzhijie <xiongzhijie@norinaviation.com>
2025-07-14 16:17:29 +0800

    refactor(k8s): :recycle: 优化Redis地址获取方式，使用CONFIG_CACHE.getValue()替代直接取值

--- datasophon-k8s/src/main/java/com/datasophon/k8s/actor/handler/K8sYamlDeploymentHandler.java
+++ datasophon-k8s/src/main/java/com/datasophon/k8s/actor/handler/K8sYamlDeploymentHandler.java
@@ -456,8 +456,8 @@ public class K8sYamlDeploymentHandler {
             extractHiveDatabaseInfo();
         }
         if ("REDIS".equals(serviceName)) {
-            String redisMasterAddr = String.valueOf(CONFIG_CACHE.get("RedisMasterAddr"));
-            String redisSlaveAddr = String.valueOf(CONFIG_CACHE.get("RedisSlaveAddr"));
+            String redisMasterAddr = (String) CONFIG_CACHE.get("RedisMasterAddr").getValue();
+            String redisSlaveAddr = (String) CONFIG_CACHE.get("RedisSlaveAddr").getValue();
             if (StrUtil.isNotBlank(redisSlaveAddr)) {
                 data.put("REDIS_ADDRESS", redisMasterAddr + "," + redisSlaveAddr);
             } else {
