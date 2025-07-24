35efc91967543bfdc2894190f293986ac3b6b65f
任相鹏 <renxiangpeng@norinaviation.com>
2025-07-24 12:30:25 +0800

    refactor(kubernetes): ♻️ 优化Kubernetes停止Pod的处理逻辑
    删除不再使用的Collectors.toList()方法，改用toList()简化代码
    调整client.pods().delete()为更规范的client.pods().resource(pod).delete()写法

--- datasophon-kubernetes/src/main/java/com/datasophon/kubernetes/actor/handler/KubernetesStopRolePodHandler.java
+++ datasophon-kubernetes/src/main/java/com/datasophon/kubernetes/actor/handler/KubernetesStopRolePodHandler.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 public class KubernetesStopRolePodHandler {
@@ -33,12 +32,14 @@ public class KubernetesStopRolePodHandler {
         logger = LoggerFactory.getLogger(loggerName);
     }
 
-    public ExecResult stop(String namespace,String kubeConfig, String hostname) {
+    public ExecResult stop(String namespace, String kubeConfig, String hostname) {
         ExecResult execResult = new ExecResult();
         try (KubernetesClient client = KubeUtil.getKubeClientByConfig(kubeConfig)) {
-            List<Pod> pods = client.pods().inNamespace(namespace).withLabel("app", serviceRoleFullName).list().getItems();
+            List<Pod> pods = client.pods().inNamespace(namespace).withLabel("app", serviceRoleFullName).list()
+                    .getItems();
             if (CollUtil.isNotEmpty(pods)) {
-                List<String> hostList = pods.stream().map(pod -> pod.getSpec().getNodeName()).collect(Collectors.toList());
+                List<String> hostList = pods.stream().map(pod -> pod.getSpec().getNodeName())
+                        .toList();
                 if (CollUtil.isEmpty(pods) || !hostList.contains(hostname)) {
                     execResult.setExecResult(true);
                     return execResult;
@@ -48,7 +49,7 @@ public class KubernetesStopRolePodHandler {
                     if (nodeName != null && nodeName.equals(hostname)) {
                         String podName = pod.getMetadata().getName();
                         logger.info("删除节点 {} 上的pod: {}", hostname, podName);
-                        client.pods().delete(pod);
+                        client.pods().resource(pod).delete();
                     }
                 }
             }
