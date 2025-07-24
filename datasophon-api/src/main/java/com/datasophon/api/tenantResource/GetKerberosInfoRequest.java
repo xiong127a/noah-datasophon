dcb9e906ff196250620ef59f210888b9ff0d5015
任相鹏 <renxiangpeng@norinaviation.com>
2025-07-24 15:52:21 +0800

    refactor(api): :fire: 移除租户资源相关的Kerberos信息请求和响应类
    删除不再使用的GetKerberosInfoRequest和GetKerberosInfoResponse类。这些类原本用于处理Kerberos相关信息的请求和响应，但当前架构中已不再需要此功能。

--- datasophon-api/src/main/java/com/datasophon/api/tenantResource/GetKerberosInfoRequest.java
+++
@@ -1,8 +0,0 @@
-package com.datasophon.worker.strategy.tenantResource;
-
-import lombok.Data;
-
-@Data
-public class GetKerberosInfoRequest {
-    private String serviceRoleName;
-}
--- datasophon-api/src/main/java/com/datasophon/api/tenantResource/GetKerberosInfoResponse.java
+++
@@ -1,8 +0,0 @@
-package com.datasophon.worker.strategy.tenantResource;
-
-import lombok.Data;
-
-@Data
-public class GetKerberosInfoResponse {
-    private String kerberosInfo;
-}
