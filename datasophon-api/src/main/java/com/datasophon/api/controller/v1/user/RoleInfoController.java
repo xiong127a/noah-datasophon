9bd07c3dc0ab62104a58549dc347b15de7ae9275
任相鹏 <renxiangpeng@norinaviation.com>
2025-07-31 13:16:03 +0800

    feat(api): ✨ 更新VersionedRequestMappingHandlerMapping以适配JDK21和现代Spring Boot版本
    本次提交主要包括以下更改：
    - 修改`VersionedRequestMappingHandlerMapping`类，简化版本化请求映射的创建逻辑，以支持JDK21及现代Spring Boot版本。
    - 更新`RoleInfoController`的包路径，并使用`@ApiVersion`注解替代`@RestController`和`@RequestMapping`，以提升API版本管理的灵活性。
    
    这些更改旨在提升代码的可维护性和API的版本管理能力。

--- datasophon-api/src/main/java/com/datasophon/api/controller/v1/user/RoleInfoController.java
+++ datasophon-api/src/main/java/com/datasophon/api/controller/v1/user/RoleInfoController.java
@@ -15,7 +15,7 @@
  *  limitations under the License.
  */
 
-package com.datasophon.api.controller;
+package com.datasophon.api.controller.v1.user;
 
 import com.datasophon.api.service.RoleInfoService;
 import com.datasophon.common.utils.Result;
@@ -26,12 +26,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.datasophon.api.annotation.ApiVersion;
 
 import java.util.Arrays;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/api/role/info")
+@ApiVersion(path = "role/info")
 public class RoleInfoController {
 
     @Autowired
