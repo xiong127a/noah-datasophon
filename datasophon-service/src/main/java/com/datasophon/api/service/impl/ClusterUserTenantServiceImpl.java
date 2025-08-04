66668f3c26408e07fbf1c4036cf8e031cf339736
任相鹏 <renxiangpeng@norinaviation.com>
2025-08-04 17:05:13 +0800

    style(api): :art: 移除尾部多余空格
    修复ClusterUserTenantServiceImpl.java文件中方法末尾的多余空格
    
    Signed-off-by: 任相鹏 <renxiangpeng@norinaviation.com>

--- datasophon-service/src/main/java/com/datasophon/api/service/impl/ClusterUserTenantServiceImpl.java
+++ datasophon-service/src/main/java/com/datasophon/api/service/impl/ClusterUserTenantServiceImpl.java
@@ -154,7 +154,7 @@ public class ClusterUserTenantServiceImpl extends ServiceImpl<ClusterUserTenantM
 
     @Override
     public List<ClusterUserTenant> getListByTenantId(Integer tenantId) {
-        return getMapper().selectByTenantId(tenantId);  
+        return getMapper().selectByTenantId(tenantId);
     }
 
 }
