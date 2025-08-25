296d86df2183856dba3a8eb1a49cb348621f3968
任相鹏 <renxiangpeng@norinaviation.com>
2025-08-25 13:26:11 +0800

    refactor(api): :truck: 调整KubernetesDashboardController包结构
    将KubernetesDashboardController从`v1/cluster`迁移到`v1/kubernetes`包，与
    其他Kubernetes相关控制器保持一致，便于维护和权限控制。
    
    Signed-off-by: 任相鹏 <renxiangpeng@norinaviation.com>

--- datasophon-api/src/main/java/com/datasophon/api/controller/v1/kubernetes/KubernetesDashboardController.java
+++ datasophon-api/src/main/java/com/datasophon/api/controller/v1/kubernetes/KubernetesDashboardController.java
@@ -15,7 +15,7 @@
  *  limitations under the License.
  */
 
-package com.datasophon.api.controller.v1.cluster;
+package com.datasophon.api.controller.v1.kubernetes;
 
 import com.datasophon.api.annotation.ClusterId;
 import com.datasophon.api.converter.K8sResourceConverter;
