81a3acb7789263c57f8d0e28719324afeff2b264
任相鹏 <renxiangpeng@norinaviation.com>
2025-10-30 16:16:50 +0800

    ✨ refactor(datasophon-service): 为ClusterHostServiceImpl中的prometheusIntegrationService字段添加@Lazy注解
    变更内容：
    1. 在`ClusterHostServiceImpl`中，为`prometheusIntegrationService`字段添加`@Lazy`注解，以优化依赖注入。
    
    效果：
    - 提升了依赖管理的灵活性，减少了不必要的初始化，提高了系统性能。

--- datasophon-service/src/main/java/com/datasophon/api/service/host/impl/ClusterHostServiceImpl.java
+++ datasophon-service/src/main/java/com/datasophon/api/service/host/impl/ClusterHostServiceImpl.java
@@ -46,6 +46,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -76,6 +77,7 @@ public class ClusterHostServiceImpl extends ServiceImpl<ClusterHostMapper, Clust
     private ClusterRackService clusterRackService;
     
     @Autowired
+    @Lazy
     private PrometheusIntegrationService prometheusIntegrationService;
     
     @Autowired
