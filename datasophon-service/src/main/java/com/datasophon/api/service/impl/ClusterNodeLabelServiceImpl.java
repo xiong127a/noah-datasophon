7b9bfafd3a8519f3ac924cc9f191d485bbb6e38f
任相鹏 <renxiangpeng@norinaviation.com>
2025-10-30 16:09:57 +0800

    ✨ refactor(datasophon-service): 使用@Lazy注解优化依赖注入
    变更内容：
    1. 在`ClusterNodeLabelServiceImpl`和`ClusterQueueCapacityServiceImpl`中，为`roleInstanceService`字段添加`@Lazy`注解，以延迟加载依赖。
    
    效果：
    - 提升了依赖管理的灵活性，减少了不必要的初始化，提高了系统性能。

--- datasophon-service/src/main/java/com/datasophon/api/service/impl/ClusterNodeLabelServiceImpl.java
+++ datasophon-service/src/main/java/com/datasophon/api/service/impl/ClusterNodeLabelServiceImpl.java
@@ -48,6 +48,7 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -80,6 +81,7 @@ public class ClusterNodeLabelServiceImpl extends ServiceImpl<ClusterNodeLabelMap
     private ClusterHostService hostService;
 
     @Autowired
+    @Lazy
     private ClusterServiceRoleInstanceService roleInstanceService;
 
     @Autowired
